f377b130f71a338c6cc585abb83ab52b5dc7cf7a
user-525 <u2102587@gmail.com>
2024-11-14 18:50:20 +0530

    url updated

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,1 +1,2 @@
-export const CURRENT_API = "http://localhost:4001";
+// export const CURRENT_API = "http://localhost:4001"; // local api
+export const CURRENT_API = "https://township-backend.onrender.com";
